a10fe2cf95c3160b05f1e1835cb9218e52dc0249
Andrew Jarrett <andrew@nanddevelopment.com>
2016-12-10 00:17:06 -0500

    Added in control variables

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ from WordPower import WordPower
 start = 2008
 end = 2008
 
+scrape = True
+process = True
+regression = True
+reports = True
+
 if __name__ == '__main__':
     wp = WordPower(start, end)
 
     print("Loading data...")
     wp.load_data()
 
-    print("Scraping SEC Edgar website for 10-Ks...")
-    wp.scrape_edgar()
+    if scrape:
+        print("Scraping SEC Edgar website for 10-Ks...")
+        wp.scrape_edgar()
+
+    if process:
+        pass
+
+    if regression:
+        pass
+
+    if reports:
+        pass
 
